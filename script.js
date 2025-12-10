bea0c793137ffce16f06b70da75921f1bf541106
oluwatimilehinabiola461-ui <oluwatimilehinabiola461@gmail.com>
2025-12-10 13:48:19 +0100

    add: second commit

--- script.js
+++ script.js
@@ -1,13 +1,51 @@
+
+const container = document.querySelector('container')
 // following navbar on scroll
 const navbar = document.getElementById('navbar');
 
 window.addEventListener('scroll', () => {
-  if (window.scrollY > 50) {
+    if (window.scrollY > 50) {
     navbar.classList.add('scrolled');
-  } else {
+    } else {
     navbar.classList.remove('scrolled');
   }
 });
+
+// getting the search input in header
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    let query = form.querySelector('input').value
+
+    tvMazeApi()
+});
+
+// getting movie from API
+
+
+
+async function tvMazeApi (query) {
+    const request = await fetch(`https://api.tvmaze.com/search/shows?q=${query}`)
+    const movies = await request.json() 
+    console.log(movies);
+    
+
+    makeImages()
+    
+}
+
+function makeImages(movies) {
+  for (let movie in movies) {
+    let src = movie.show.image.medium;
+
+    const img = document.createElement("img")
+    img.src-src;
+
+    container.appendChild(img);
+  }
+    
+}
 
 // featured movies carousel
 const slides = document.querySelector('.slides');
